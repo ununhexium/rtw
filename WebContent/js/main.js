05d8af2c423f99f31cca00505f7b3b2656f7681c
116 <116@lab0.net>
2015-07-25 21:08:07 +0200

    Fix modal window google map resize Fix windows resize problem with google maps

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,17 +1,19 @@
 var g = {};
 
-$(function() {
+$(document).ready(function() {
   loadRecursive($(this));
-  addGoogleMapListeners();
   initTabs();
+  addGoogleMapListeners();
 });
 
-/*
- * function initTabs() { $("a.tabs").click(function(e) { var target =
- * $(this).data("target"); var isTargetExpanded = $(target).hasClass('in');
- * $(target).removeClass('collapse').addClass("in"); // expand clicked target
- * }); }
- */
+function initTabs() {
+  $("a.tabs").click(function(e) {
+    var target = $(this).data("target");
+    var isTargetExpanded = $(target).hasClass('in');
+    $(target).removeClass('collapse').addClass("in"); // expand clicked target
+  });
+}
+
 // stackoverflow.com/questions/17908296/jquery-recursive-loading-nested-html
 function loadRecursive(context) {
   // search matching elements that have 'place-holder' class
@@ -25,39 +27,38 @@ function loadRecursive(context) {
   });
 }
 
-function addGoogleMapListeners() {
-  $('#map_modal').on('shown.bs.modal', function() {
-    setTimeout(function() {
-      google.maps.event.trigger(window.googleMap, "resize");
-    }, 500);
-  })
-
-  $('button').click(function() {
-    var clicked = $(this);
-    console.log(getDomPath(clicked).join(' > '));
-    alert(clicked);
-    
-    geocoder = new google.maps.Geocoder();
-    var address = clicked.attr('address');
-    // input box value
-    alert(address);
-    geocoder.geocode({
-      'address' : address
-    }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        window.googleMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map : window.googleMap,
-          position : results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful  for the following reason: " + status);
-      }
-    });
-  })
+function resizeGoogleMap(map) {
+  var center = map.getCenter();
+  google.maps.event.trigger(map, "resize");
+  map.setCenter(center);
 }
 
-function initialize() {
+function addGoogleMapListeners() {
+  setTimeout(function() {
+    $('button.googlemapbutton').click(function() {
+      var clicked = $(this);
+      geocoder = new google.maps.Geocoder();
+      var address = clicked.attr('address');
+      // input box value
+      alert(address);
+      geocoder.geocode({
+        'address' : address
+      }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          g.modalmap.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map : g.modalmap,
+            position : results[0].geometry.location
+          });
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
+    });
+  }, 1000);
+}
+
+function initializeGoogleMaps() {
   var mapOptions = {
     center : {
       lat : 0,
@@ -66,21 +67,25 @@ function initialize() {
     zoom : 11,
     mapTypeId : google.maps.MapTypeId.ROADMAP
   };
-  g.googleMap = new google.maps.Map(document.getElementById('map_div'), mapOptions);
+  g.modalmap = new google.maps.Map(document.getElementById('map_div'), mapOptions);
   
   mapOptions.zoom = 1;
   g.roadmap = new google.maps.Map(document.getElementById('roadmap_div'), mapOptions);
   
+  /*
+   * Refresh the roadmap on the first click on it.
+   */
   $('#tabs_div').click(function() {
     setTimeout(function() {
-      if (!g.roadmap_resized) {
-        var center = g.roadmap.getCenter();
-        google.maps.event.trigger(g.roadmap, "resize");
-        g.roadmap.setCenter(center);
-        g.roadmap_resized = true;
-      }
-    }, 1000);
+      resizeGoogleMap(g.roadmap);
+    }, 500);
+  });
+  
+  $('#map_modal').on('show.bs.modal', function() {
+    setTimeout(function() {
+      resizeGoogleMap(g.modalmap);
+    }, 500);
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initializeGoogleMaps);
